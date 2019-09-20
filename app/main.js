2e8dffb251e28a98d0e90f17a18ddcd8e1294ac1
鈴木 友梨 <suzuki-yuri@team-lab.com>
2019-09-20 16:01:46 +0900

    firebase sample-1

---
+++ app/main.js
@@ -0,0 +1,40 @@
+// メッセージングオブジェクトの取得
+const messaging = firebase.messaging();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./firebase-messaging-sw.js').then(registration => {    
+    console.log(registration);
+    messaging.useServiceWorker(registration);
+  }).catch(error => {
+    console.error(error);
+  });
+}
+
+window.addEventListener('online', e => {
+    console.log('online');
+}, false);
+
+window.addEventListener('offline', e => {
+    console.log('offline');
+}, false);
+
+// アプリがフォアグラウンドにある場合にプッシュ通知が届いた場合にログ出力
+// https://firebase.google.com/docs/cloud-messaging/js/receive?hl=ja
+messaging.onMessage(payload => {
+    console.log(payload);
+});
+
+// ボタン押下のタイミングでユーザに通知権限を求める
+function requestPermission() {
+    messaging.requestPermission().then(() => {
+        messaging.getToken().then(token => {
+            console.log(token);
+            const getToken = document.getElementById('token');
+            getToken.innerHTML = token;
+        }).catch(error => {
+            console.error(error);
+        });
+    }).catch(error => {
+      console.error(error)
+    });
+}
