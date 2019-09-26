02bdea41c9e1bcacaed5e8092d5244f17e747eaf
鈴木 友梨 <suzuki-yuri@team-lab.com>
2019-09-26 12:04:19 +0900

    fix

--- app/push.js
+++ app/push.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
-var key = 'サーバーキー';
-var to = 'トークン';
+var key = 'AAAAeucTVq8:APA91bFzhpJOJXNmSL8RGtKWG8ylwHlQDBKyQtHfDzF8LSlEDwXmyYK0bUx7urpzVVq_k7WMGHuXYZk9VtnAl0hiIrIyM83mkoQHsrCVza7zv6BetCWcrwDAXW8tIzQ00rsPRZ2BQJNH';
+var to = 'cfkNs5UDQD0:APA91bGNwmTeUp2Wf2SdH7JmVui_uojPzeiDL7jtiC9IEII4e8uDAPKaTU4dvSr_22WYhY0K5IkFrqQlDWhYTzjhezq_gwdguXUGEBRxSGz1WKsy6rx9Yl50MJAnV7zYeqdMOU8yEXJc';
 var notification = {
   'title': 'プッシュ通知',
   'body': 'テストテストテスト',
