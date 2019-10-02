93e59730f530069490a7b80900259e1422d1eccb
鈴木 友梨 <suzuki-yuri@team-lab.com>
2019-10-02 19:24:24 +0900

    トークンをDBに保存

--- app/firebase-messaging-sw.js
+++ app/firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-// Firebase SDK
 importScripts('/__/firebase/5.5.6/firebase-app.js');
 importScripts('/__/firebase/5.5.6/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
@@ -6,11 +5,8 @@ importScripts('/__/firebase/init.js');
 // メッセージングオブジェクトの取得
 var messaging = firebase.messaging();
 
-// [START background_handler]アプリがバックグラウンドで動作している場合
-// https://firebase.google.com/docs/cloud-messaging/js/first-message?authuser=1
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-
   var notificationTitle = 'Background Message Title';
   var notificationOptions = {
     body: 'Background Message body.',
